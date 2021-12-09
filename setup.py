31c80362df6cabb8a09d002306d16c39c425037e
Timothée Stachnick <timothee95@hotmail.fr>
2021-12-09 13:23:24 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
-    name='src',
+    name='utt-workshop',
     packages=find_packages(),
     version='0.1.0',
-    description='utt cookiecutter workshop',
-    author='Romain DESSAINT',
+    description=long_description,
+    long_description_content_type="text/markdown",
+    author='Best groupe',
     license='',
+    url="https://github.com/RomainDessaint/utt-workshop",
+    python_requires=">=3.6"
 )
