7e80cfa0a806d6b512bca7db9c65b8c07765bf6c
WBI <63566474+WBI77@users.noreply.github.com>
2021-12-09 13:33:25 +0100

    Update init_flask.py

--- src/feature-api/init_flask.py
+++ src/feature-api/init_flask.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/hello/', methods=['GET', 'POST'])
+@app.route('/ping/', methods=['GET'])
+
 def welcome():
     return "Hello World!"
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=105)
